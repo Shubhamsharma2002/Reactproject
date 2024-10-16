43dc8584402f97dc78954dd2dd9474c63735ef48
shubham sharma <shubhamtech004@gmail.com>
2024-10-17 00:35:35 +0530

    adding the project setup and folder strucuter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import val from '../src/conf/confing.js'
 import './App.css'
+import { useDispatch } from 'react-redux';
+import authSrvice from './Service/auth.js'
 
 function App() {
-   console.log(val.appwriteBucketId);
+   const[loading , setLoading] = useState(true);
+   const dispatch = useDispatch();
   return(
 
-    <h1>A bolg app with appwrite ${}</h1>
+    <h1>A bolg app with appwrite ${val.appwriteBucketId}</h1>
     // <p>value : {import.meta.env.VITE_hint}</p>
   )
 }
